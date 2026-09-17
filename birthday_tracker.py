38761273ba575c2e602d486ba60c6cc93b35d984
mabia <mabia@PCKIMUILI>
2026-09-17 07:45:50 +0200

    adding comments in the project

--- birthday_tracker.py
+++ birthday_tracker.py
@@ -1,9 +1,12 @@
 import calendar 
-import datetime
-year = input("Enter the year of birth: ")
+import datetime #Adding libraries to have access to calendar and datetime functions 
+year = input("Enter the year of birth: ") #Enter the year of the birth
 year = int(year)
-print(calendar.calendar(year))
+print(calendar.calendar(year)) #show the calendar of the year of birth
 now = datetime.datetime.now()
-print(now)
+print(now) #get the current date and time
+event = input("Enter the event: ")
+print(event) #have access to an event that the user want to add to the calendar
+
 
 
